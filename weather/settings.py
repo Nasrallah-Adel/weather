d54e8a9cc52b02e52cea2d4b7722cce4f9339840
Nasrallah-Adel <naseradel54@gmail.com>
2020-01-20 23:04:09 +0200

    setup rest_framework

--- weather/settings.py
+++ weather/settings.py
@@ -35,7 +35,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # <-- And here
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
